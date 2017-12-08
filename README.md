edf970176dc5461c31d0d6cf13a4e6c304066229
Henrique Rocha <hdlopesrocha91@gmail.com>
2017-12-08 23:18:07 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# gdax.email
+GDAX discord notifications
+
+This app fetches your gdax account every 30 seconds. 
+The newest fills are sent via discord.
+
+* Create a GDAX API key.
+
+* Add your credentials to **application.properties** 
+
+* Install dependencies:
+```
+npm install express --save
+npm install gdax --save
+npm install nodemailer --save
+npm install play-sound --save
+npm install properties-reader --save
+```
+
+* Run with:
+```
+nodejs app.js
+```
+
+
+Happy trading.
+
+Feel free to donate some BTC:
+1ML9M3VJ9uh79sGV3QmYxihWr1XCancUyT
+
---
+++ app.js
@@ -0,0 +1,112 @@
+// npm install express --save
+// npm install gdax --save
+// npm install --save discord.js
+// npm install properties-reader --save
+
+const fs = require('fs');
+const express = require('express');
+const Gdax = require('gdax');
+var PropertiesReader = require('properties-reader');
+var propsDir = process.argv.length == 2? 'application.properties' :  process.argv[2];
+const request = require('request');
+
+
+var properties = PropertiesReader(propsDir);
+
+const authenticatedClient = new Gdax.AuthenticatedClient(
+	properties.get('gdax.key'),
+	properties.get('gdax.secret'),
+	properties.get('gdax.passphrase'),
+	'https://api.gdax.com'
+);
+
+
+
+function saveDB(db){
+	fs.writeFile('data.json', JSON.stringify(db));
+}
+
+function loadDB(){
+	if (fs.existsSync('data.json')) {
+		return JSON.parse(fs.readFileSync('data.json'));
+	}
+	return [];
+}
+
+function getOrder(orders,id){
+	for(var i in orders){
+		var order = orders[i];
+		if(order.trade_id === id){
+			return order;
+		}
+	}
+	return null;
+}
+
+function sendMessage(text){
+	
+	request.post(properties.get('discord.webhook'), {
+		form: {
+		 	content:text
+		}
+	});	
+}
+
+function collector(){
+
+	authenticatedClient.getFills(
+		{},
+		(error, response, orders) => {
+  		if (!error && response.statusCode == 200) {
+				console.log('getFills',new Date());
+				var database = loadDB();
+				if(database.length == 0){
+					database = orders;
+					saveDB(database);
+				}
+				
+				var fills = [];
+				var hasNew = false;
+
+				for(var i in orders){
+					var oid = orders[i].trade_id;
+					var curr = getOrder(orders,oid);						
+					var prev = getOrder(database,oid);
+				
+					if(prev == null) {
+						hasNew = true;
+					}
+					curr.isNew = prev == null;
+				
+					var d = new Date(curr.created_at);
+					var c = new Date();
+									
+					if(c.getTime() - d.getTime() < 1000*60*60*24) {
+
+						fills.push(curr);
+					}
+				}
+
+				if(hasNew){
+					var message = '';
+					for(var i in fills){
+						var fill = fills[i];
+						if(fill.isNew){
+							message += fill.side +' '+ fill.product_id+' at price='+fill.price +', size='+fill.size +'\n';
+						}
+					}
+					sendMessage(message);
+					saveDB(orders);
+				}
+			}else{
+				console.log('error',new Date());
+			}
+		}
+	);
+	setTimeout(collector, 30000);
+}
+
+  collector();
+
+
+
